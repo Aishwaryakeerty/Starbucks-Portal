7459f6be4baa4d5325a744d0fe9989891d3b0d78
Aishwaryakeerty <aishwarya.keerty@sjsu.edu>
2017-05-13 15:39:39 -0700

    Update homeController.js

--- angularjs/homeController.js
+++ angularjs/homeController.js
@@ -1,4 +1,7 @@
 starbucks.controller('homeController', function ($scope,$http,$rootScope) {
 	if($rootScope.login == null)
 		$rootScope.login = "Login";
+	$scope.OnItemClick = function(event) {
+		 	alert(event);
+		 };
 });
